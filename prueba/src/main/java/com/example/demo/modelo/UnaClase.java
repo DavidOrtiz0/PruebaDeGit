99b080d59a66ad3c08c85fe4383e0aa6b7ec5262
jokerdown <jokerdown@IdeaPad-3-14ALC6-756af8b5.localdomain>
2024-02-23 18:26:31 -0500

    ultimo cambio

--- prueba/src/main/java/com/example/demo/modelo/UnaClase.java
+++ prueba/src/main/java/com/example/demo/modelo/UnaClase.java
@@ -5,5 +5,7 @@ public class UnaClase {
 	//algun cambio tiene que aparecer
 	int valor = 0;
 	//realizando otro cambio
-	Steing nombre = "David";
+	String nombre = "David";
+	//ultimo cambio
+	char Valor = 'S';
 }
